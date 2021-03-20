255c7a86b081335efa17d377455120a2f6f14ca2
Jason S <jcspeare@gmail.com>
2021-03-20 16:27:50 -0700

    Testing easygraphQL test creation

--- src/components/TestSuite/GraphQLEndpoint/GraphQLEndpoint.tsx
+++ src/components/TestSuite/GraphQLEndpoint/GraphQLEndpoint.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext} from "react";
+import React, { useState, useContext } from "react";
 // import GraphQL from "graphql";
 // @ts-ignore
 import { ApolloClient, InMemoryCache, ApolloProvider, HTTPLink, gql, from } from "@apollo/client/core";
@@ -7,59 +7,82 @@ import { StateContext } from "../../../provider/StateProvider";
 import Input from '../../../../node_modules/@material-ui/core/Input';
 // @ts-ignore
 import { AwesomeButton, AwesomeButtonProgress, AwesomeButtonSocial } from 'react-awesome-button';
+
 const { remote } = window.require('electron');
 const GraphQl = () => {
-    const { test, testHandler }: any = useContext(TestContext);
-    const { activePort, activeFile, setActiveFile }: any = useContext(StateContext)
-    const [local, localhandler] = useState({});
-    let current;
+  const { test, testHandler }: any = useContext(TestContext);
+  const { activePort, activeFile, setActiveFile }: any = useContext(StateContext)
+  const [local, localhandler] = useState({});
+  let current;
 
-    const methods = ["GET", "POST", "PUT", "DELETE"];
-    const methodOptions = [];
-    for (let a = 0; a < methods.length; a++) {
-      methodOptions.push(
-        <option key={`${a} + method`} value={methods[a]}>
-          {methods[a]}
-        </option>
-      );
-    }
-    const getPath = () => { remote.dialog
-      .showOpenDialog({ properties: ['openFile'], message: 'Please choose your server file'})
+  const methods = ["GET", "POST", "PUT", "DELETE"];
+  const methodOptions = [];
+  for (let a = 0; a < methods.length; a++) {
+    methodOptions.push(
+      <option key={`${a} + method`} value={methods[a]}>
+        {methods[a]}
+      </option>
+    );
+  }
+  const getPath = () => {
+    remote.dialog
+      .showOpenDialog({ properties: ['openFile'], message: 'Please choose your server file' })
       .then((files: any) => {
         if (!files.cancelled) {
-           current = files.filePaths[0];
-          inputHandler(null)}
-          })
+          current = files.filePaths[0];
+          inputHandler(null)
         }
-    const getSchemaPath = () => { remote.dialog
-        .showOpenDialog({ properties: ['openFile'], message: 'Please choose your schema file'})
-        .then((files: any) => {
-          if (!files.cancelled) {
-             current = files.filePaths[0];
-            inputHandler(undefined)}
-            })
-          }        
+      })
+  }
+  const getSchemaPath = () => {
+    remote.dialog
+      .showOpenDialog({ properties: ['openFile'], message: 'Please choose your schema file' })
+      .then((files: any) => {
+        if (!files.cancelled) {
+          current = files.filePaths[0];
+          inputHandler(undefined)
+        }
+      })
+  }
 
-    const selectHandler = (e: any): void => {
-      const dropdownId = e.target.id;
-      const dropdown = document.getElementById(dropdownId) as HTMLSelectElement;
-      const selection = dropdown.options[dropdown.selectedIndex].text;
-      testHandler(dropdownId, selection);
-    }
+  const selectHandler = (e: any): void => {
+    const dropdownId = e.target.id;
+    const dropdown = document.getElementById(dropdownId) as HTMLSelectElement;
+    const selection = dropdown.options[dropdown.selectedIndex].text;
+    testHandler(dropdownId, selection);
+  }
 
-    const inputHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
-      if (e == null) {
-        console.log({'serverApp': current})
-        testHandler('serverApp', current)
-      } else if (e == undefined) {
-        console.log({'schemaApp': current})
-        testHandler('schemaApp', current)
-      } else
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    if (e == null) {
+      console.log({ 'serverApp': current })
+      testHandler('serverApp', current)
+    } else if (e == undefined) {
+      console.log({ 'schemaApp': current })
+      testHandler('schemaApp', current)
+    } else
       testHandler(e.target.id, (e.target as HTMLInputElement).value);
-    };
-    
-    return (
-      <div>
+  };
+
+//endpoint test
+  const queryValidity = () => {
+    const newTest = `describe('${test.expectedRes}', () => {
+      it('${test.expectedRes}', () => {
+        post(${test.inputData});
+      tester.test(${true}, ${test.outputData})
+    })`;
+    console.log('i got clicked')
+    console.log(newTest)
+    return newTest;
+
+
+
+
+
+  }
+    //end of endpoint test edits
+
+  return (
+    <div>
       <i className="fas fa-vial"></i>
       <div id='test-title'>Test Builder</div>
       <div className='rest-endpoint'>
@@ -70,53 +93,54 @@ const GraphQl = () => {
           {methodOptions}
         </select>
         <b></b>
-              <div id='server-button'>
-              <AwesomeButtonProgress 
-                  type="secondary"
-                  ripple={true}
-                  
-                  action={(element, next) => {
-                      getPath()
-                      setTimeout(() => {
-                        next();
-                      }, 1000);
-                      return 
-                    }}
-                  loadingLabel='connecting'
-                  resultLabel='connected'
-                  >
-                  UPLOAD SERVER FILE
+        <div id='server-button'>
+          <AwesomeButtonProgress
+            type="secondary"
+            ripple={true}
+
+            action={(element, next) => {
+              getPath()
+              setTimeout(() => {
+                next();
+              }, 1000);
+              return
+            }}
+            loadingLabel='connecting'
+            resultLabel='connected'
+          >
+            UPLOAD SERVER FILE
               </AwesomeButtonProgress>
-              <AwesomeButtonProgress 
-                  type="secondary"
-                  ripple={true}
-                  
-                  action={(element, next) => {
-                    getSchemaPath()
-                      setTimeout(() => {
-                        next();
-                      }, 1000);
-                      return 
-                    }}
-                  loadingLabel='connecting'
-                  resultLabel='connected'
-                  >
-                  UPLOAD SCHEMA FILE
+          <AwesomeButtonProgress
+            type="secondary"
+            ripple={true}
+
+            action={(element, next) => {
+              getSchemaPath()
+              setTimeout(() => {
+                next();
+              }, 1000);
+              return
+            }}
+            loadingLabel='connecting'
+            resultLabel='connected'
+          >
+            UPLOAD SCHEMA FILE
               </AwesomeButtonProgress>
-              <Input placeholder='Database URI' fullWidth={true} id="URI" type='password' onChange={inputHandler}/>  
-              </div>
-        <br/>
-        <br/>
-        <Input placeholder='Describe Your Test' fullWidth={true} id="expectedRes" type='text' onChange={inputHandler}/>  
-        <Input placeholder='Route/Endpoint' defaultValue={test.desiredEndpoint || ''} fullWidth={true} id="desiredEndpoint" type='text' onChange={inputHandler}/>  
-        <Input placeholder='Query Input' defaultValue={test.inputData || ''} fullWidth={true} id="inputData" type='text' onChange={inputHandler}/>  
-        <Input placeholder='Assertion' defaultValue={test.outputData || ''} fullWidth={true} id="outputData" type='text' onChange={inputHandler}/>  
-        <Input placeholder='Header' defaultValue={test.headerInfo || ''} fullWidth={true} id="headerInfo" type='text' onChange={inputHandler}/>  
+          <Input placeholder='Database URI' fullWidth={true} id="URI" type='password' onChange={inputHandler} />
+        </div>
+        <br />
+        <br />
+        <Input placeholder='Describe Your Test' defaultValue={test.expectedRes || ''}fullWidth={true} id="expectedRes" type='text' onChange={inputHandler} />
+        <Input placeholder='Route/Endpoint' defaultValue={test.desiredEndpoint || ''} fullWidth={true} id="desiredEndpoint" type='text' onChange={inputHandler} />
+        <Input placeholder='Query Input' defaultValue={test.inputData || ''} fullWidth={true} id="inputData" type='text' onChange={inputHandler} />
+        <Input placeholder='Assertion' defaultValue={test.outputData || ''} fullWidth={true} id="outputData" type='text' onChange={inputHandler} />
+        <Input placeholder='Header' defaultValue={test.headerInfo || ''} fullWidth={true} id="headerInfo" type='text' onChange={inputHandler} />
         <br></br>
+        <button onClick={() => queryValidity()}>Test button</button>
       </div>
-      </div>
-    );
-  };
+    </div>
+  );
+};
 
 
 export default GraphQl;
