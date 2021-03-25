ce81ac4a7a6eaa1bce756a630805bb152d324380
Kim S <kspicerny@gmail.com>
2021-03-24 20:26:22 -0400

    updated test suite, more work to be done.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // The root of your source code, typically /src
     // `<rootDir>` is a token Jest substitutes
-    roots: ["<rootDir>/src"],
+    roots: ["<rootDir>"],
   
     // Jest transformations -- this adds support for TypeScript
     // using ts-jest
@@ -19,13 +19,15 @@ module.exports = {
     //   "@testing-library/jest-dom/extend-expect"
     // ],
 
-
-
+    // Sample regex
+    // "(/__tests__/.*|(\\.|/)(test|spec))\\.js?$",  (/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$' ,  [ "/__tests__/.*\\.(ts|tsx|js),
   
     // Test spec file resolution pattern
     // Matches parent folder `__tests__` and filename
     // should contain `test` or `spec`.
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
+
+    
   
     // Module file extensions for importing
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
---
+++ src/__tests__/FileTree.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+
+import FileTree from "../components/FileTree/FileTree";
+import Navbar from "../components/NavBar/NavBar"
+
+describe("<Navbar />", () => {
+  test("should nav", async () => {
+    // ???
+  });
+});
